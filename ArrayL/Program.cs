87cad2945504bba684d291a0918030e024e02822
irbis16 <irbis16@yandex.ru>
2022-12-06 19:48:35 +0300

    код из конспекта

--- ArrayL/Program.cs
+++ ArrayL/Program.cs
@@ -1,8 +1,30 @@
 ﻿int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
-void PrintArray(int[] array);
+void PtintArray(int[] array)
 {
-int count = array.Length;
+int count = array.Length
 for (int i = 0; i < count; i++)
-Console.Write($"{array[i]}");
+{
+Console.Write($”{array[i]}”);
+}
 Console.WriteLine();
 }
+void selectionSort(int[] array)
+{
+for (int i = 0; i < array.Length - 1; i++)
+{
+int minPosition = i;
+for (int j = i+1; j < array.Length; j++)
+{
+if(array[j] < array[minPosition]) minPosition = j;
+{
+minPosition = j
+}
+}
+int temporary = array[i]
+array[i] = array[minPosition];
+array[minPosition] = temporary;
+}
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
